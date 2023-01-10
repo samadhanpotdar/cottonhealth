6ddbf97d1b563fcaabc1604866f6efe2a686b434
samadhanpotdar <122345784+samadhanpotdar@users.noreply.github.com>
2023-01-10 13:05:40 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,122 @@
+$(function () {
+
+    $('#preloader').fadeOut();
+
+
+    
+
+    'use strict';
+    $(window).on('load', function (event) {
+        $('.preloader').delay(500).fadeOut(500);
+    });
+    //===== Sticky
+
+    $(window).on('scroll', function (event) {
+
+        if ($(window).scrollTop() < 20) {
+            $(".nav-bar").removeClass("sticky");
+        } else {
+            $(".nav-bar").addClass("sticky");
+        }
+    });
+
+
+
+ //======preloader
+
+ window.onload = function () {
+    window.setTimeout(fadeout, 500);
+}
+
+function fadeout() {
+    document.querySelector('.preloader').style.opacity = '0';
+    document.querySelector('.preloader').style.display = 'none';
+}
+
+
+
+
+ //===== Counter Up
+
+ $('.counter').counterUp({
+    delay: 10,
+    time: 1600,
+});
+
+
+
+
+
+
+
+    //===== Section Menu Active
+
+    var scrollLink = $('.page-scroll');
+    // Active link switching
+    $(window).scroll(function () {
+        var scrollbarLocation = $(this).scrollTop();
+
+        scrollLink.each(function () {
+
+            var sectionOffset = $(this.hash).offset().top - 73;
+
+            if (sectionOffset <= scrollbarLocation) {
+                $(this).parent().addClass('active');
+                $(this).parent().siblings().removeClass('active');
+            }
+        });
+    });
+
+
+    //===== close navbar-collapse when a  clicked
+
+    $(".navbar-nav a").on('click', function () {
+        $(".navbar-collapse").removeClass("show");
+    });
+
+    $(".navbar-toggler").on('click', function () {
+        $(this).toggleClass("active");
+    });
+
+    $(".navbar-nav a").on('click', function () {
+        $(".navbar-toggler").removeClass('active');
+    });
+
+    var wow = new WOW({
+        boxClass: 'wow', // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset: 0, // distance to the element when triggering the animation (default is 0)
+        mobile: true, // trigger animations on mobile devices (default is true)
+        live: true, // act on asynchronously loaded content (default is true)
+        callback: function (box) {
+            // the callback is fired every time an animation is started
+            // the argument that is passed in is the DOM node being animated
+        },
+        scrollContainer: null // optional scroll container selector, otherwise use window
+    });
+    wow.init();
+
+
+
+
+     /* Back Top Link active
+    ========================================================*/
+    var offset = 200;
+    var duration = 500;
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > offset) {
+        $('.back-to-top').fadeIn(400);
+      } else {
+        $('.back-to-top').fadeOut(400);
+      }
+    });
+
+    $('.back-to-top').on('click',function(event) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: 0
+      }, 200);
+      return false;
+    });
+
+});
